1dfa32df21e844ce7cd5b1e963749634114da8b1
OlekZima <zimenkoalex34@gmail.com>
2026-03-23 11:43:30 +0100

    Remove unused scripts

--- src/utils/spark.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-def get_spark():
-    return (
-        SparkSession.builder.appName("BGD")
-        .config("spark.sql.shuffle.partitions", "8")
-        .getOrCreate()
-    )
